09b74dfe70da6be30d6a7039452e04d6cab9179d
zehreken <zehreken@gmail.com>
2019-05-23 21:47:59 +0200

    Add SDL and SDL_image frameworks

--- cellular_automata/cellular_automata/main.cpp
+++ cellular_automata/cellular_automata/main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  cellular_automata
-//
-//  Created by Guchan Alkan on 23.05.2019.
-//  Copyright © 2019 zehreken. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
