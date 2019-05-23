57602889a5263c2f49cc6c4f1d207f4b9a006056
zehreken <zehreken@gmail.com>
2019-05-23 21:11:44 +0200

    Add initial files

---
+++ cellular_automata/cellular_automata/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cellular_automata
+//
+//  Created by Guchan Alkan on 23.05.2019.
+//  Copyright © 2019 zehreken. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
