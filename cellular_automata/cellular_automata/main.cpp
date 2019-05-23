c39b100115c5123cae8c686217af82d78f77b810
zehreken <zehreken@gmail.com>
2019-05-23 22:16:13 +0200

    Add window creation

--- cellular_automata/cellular_automata/main.cpp
+++ cellular_automata/cellular_automata/main.cpp
@@ -1,7 +1,81 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+SDL_Window *p_window;
+SDL_Renderer *p_renderer;
+const int SCREEN_WIDTH = 512;
+const int SCREEN_HEIGHT = 512;
+
+bool init()
+{
+	bool success = true;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		p_window = SDL_CreateWindow("cellular_automata", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (p_window == NULL)
+		{
+			printf("Window could not be created. SDL Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			p_renderer = SDL_CreateRenderer(p_window, -1, SDL_RENDERER_ACCELERATED);
+			if (p_renderer == NULL)
+			{
+				printf("Renderer could not be created. SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+				SDL_SetRenderDrawColor(p_renderer, 0, 0, 0, 255);
+				
+//				int imgFlags = IMG_INIT_PNG;
+//				if (!(IMG_Init(imgFlags) & imgFlags))
+//				{
+//					printf( "SDL_image could not initialize. SDL_image Error: %s\n", IMG_GetError() );
+//					success = false;
+//				}
+			}
+		}
+	}
+	
+	return success;
+}
+
+void update()
+{
+	SDL_RenderClear(p_renderer);
+	
+	SDL_Delay(16);
+}
+
+int main(int argc, const char * argv[])
+{
+	if (!init())
+	{
+		printf("Error initializing\n");
+		return -1;
+	}
+	
+	bool quit = false;
+	while (!quit)
+	{
+		SDL_Event e;
+		while(SDL_PollEvent( &e ) != 0)
+		{
+			if(e.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+		
+		update();
+	}
+	
+	return 0;
 }
