8d4ecefa03c8d7e7ce377776e3fdd0da4894895e
zehreken <zehreken@gmail.com>
2019-05-28 19:36:31 +0200

    Add new project, perlin_noise

---
+++ perlin_noise/perlin_noise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  perlin_noise
+//
+//  Created by Guchan Alkan on 28.05.2019.
+//  Copyright © 2019 zehreken. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
